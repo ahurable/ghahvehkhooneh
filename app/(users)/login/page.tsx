e98cb44c2a64a50f0f5cb79a6d9a9e5d7513e4f3
ahura alipour <kingahoora23@gmail.com>
2024-01-04 16:04:05 -0800

    Register (working on it)

--- app/(users)/login/page.tsx
+++ app/(users)/login/page.tsx
@@ -2,11 +2,19 @@
 
 const Login = () => {
     return (
-        <main className="w-full h-[100vh] bg-amber-200">
-            <div className="md:w-[720px] lg:w-[1000px] w-100 md:ms-40 p-4 bg-white h-full flex justify-center text-center">
-                <h1 className="text-3xl font-black text-brown-dark pt-10">ورود به حساب</h1>
-                <div className="flex justify-centent-center">
-                    <form action=""></form>
+        <main className="w-full  relative bg-purple-600">
+            <div className="md:w-[720px] lg:w-[1000px] w-11/12 mx-auto relative h-full">
+                <h1 className="text-center text-[34px] pt-10 text-white font-bold">قهوه خونه</h1>
+                <h1 className="text-center text-[50px] pb-10 font-black text-white">ایجاد حساب</h1>
+                <div className="form-wrapper">
+                    <div className="flex justify-centent-center">
+                        <form action="">
+                            <input type="text" id="name" name="name" className="form-control mx-2 md:w-[420px] w-full mt-20" placeholder="نام خود  را بنویسید" />
+                            <input type="text" id="name" name="name" className="form-control mx-2 md:w-[420px] w-full mt-4" placeholder="نام خود  را بنویسید" />
+                            <input type="text" id="name" name="name" className="form-control mx-2 md:w-[420px] w-full mt-4" placeholder="نام خود  را بنویسید" />
+                            <input type="text" id="name" name="name" className="form-control mx-2 md:w-[420px] w-full mt-4" placeholder="نام خود  را بنویسید" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </main>
