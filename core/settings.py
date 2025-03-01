8eed3185f59452c6ae61d1a7f666dfd033816e80
ahura <ahura.alipur19@yahoo.com>
2025-03-01 16:19:00 -0500

    added db

--- core/settings.py
+++ core/settings.py
@@ -91,13 +91,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gappy',  # Replace with your database name
+        'USER': 'gappy',  # Replace with your database username
+        'PASSWORD': '5@5$32341jdlskf0',  # Replace with your database password
+        'HOST': 'db',  # Change to your DB host (or use 'db' if using Docker)
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
