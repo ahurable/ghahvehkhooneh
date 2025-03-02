5c38ef3241b2a0ab891b5625585e4599ace67f00
ahura <ahura.alipur19@yahoo.com>
2025-03-02 09:02:46 -0500

    ready for deploy

--- lib/variebles.ts
+++ lib/variebles.ts
@@ -1,5 +1,7 @@
 
 
 // export const LOCALHOST = 'http://192.168.195.81:8000/'
-export const LOCALHOST = 'http://127.0.0.1:8000/'
-export const IMAGE_HOST = 'http://127.0.0.1:8000'
+// export const LOCALHOST = 'http://127.0.0.1:8000/'
+// export const IMAGE_HOST = 'http://127.0.0.1:8000'
+export const LOCALHOST = 'https://backendgappy.darkube.app/'
+export const IMAGE_HOST = 'https://backendgappy.darkube.app'
