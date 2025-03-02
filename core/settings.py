cb300169e3d2215d61b8b457234c209edf5e9b3d
ahura <ahura.alipur19@yahoo.com>
2025-03-02 17:03:29 -0500

    switched to dj_database_url again

--- core/settings.py
+++ core/settings.py
@@ -103,16 +103,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),                      
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
-        'PORT': '5432',
-    }
-}
+DATABASES["default"] = dj_database_url.config(
+    default=os.getenv('DB_URL'),
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
