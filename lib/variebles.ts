e1b27a5859a1ff1ccacbb8a24e748f1236ca036b
ahura <ahura.alipur19@yahoo.com>
2025-03-02 18:59:12 -0500

    fixed backend address

--- lib/variebles.ts
+++ lib/variebles.ts
@@ -3,5 +3,5 @@
 // export const LOCALHOST = 'http://192.168.195.81:8000/'
 // export const LOCALHOST = 'http://127.0.0.1:8000/'
 // export const IMAGE_HOST = 'http://127.0.0.1:8000'
-export const LOCALHOST = 'https://backendgappy.darkube.app/'
-export const IMAGE_HOST = 'https://backendgappy.darkube.app'
+export const LOCALHOST = 'http://94.101.184.56:8000/'
+export const IMAGE_HOST = 'http://94.101.184.56:8000'
