75fd5c1acc80cfdcb9201ef3775beaee043d5653
ahura <ahura.alipur19@yahoo.com>
2025-03-03 03:40:50 -0500

    pushed commits

--- lib/variebles.ts
+++ lib/variebles.ts
@@ -1,7 +1,7 @@
 
 
 // export const LOCALHOST = 'http://192.168.195.81:8000/'
-export const LOCALHOST = 'http://127.0.0.1:8000/'
-export const IMAGE_HOST = 'http://127.0.0.1:8000'
-// export const LOCALHOST = 'http://94.101.184.56:8000/'
-// export const IMAGE_HOST = 'http://94.101.184.56:8000'
+// export const LOCALHOST = 'http://127.0.0.1:8000/'
+// export const IMAGE_HOST = 'http://127.0.0.1:8000'
+export const LOCALHOST = 'http://94.101.184.56:8000/'
+export const IMAGE_HOST = 'http://94.101.184.56:8000'
