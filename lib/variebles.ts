eb98835d204b12a0b269949b550b8647e9305ede
ahura <ahura.alipur19@yahoo.com>
2025-03-12 07:19:51 -0400

    fixed vars

--- lib/variebles.ts
+++ lib/variebles.ts
@@ -1,10 +1,10 @@
 
 
 // export const LOCALHOST = 'http://192.168.195.81:8000/'
-export const LOCALHOST = 'http://127.0.0.1:8000/'
-export const IMAGE_HOST = 'http://127.0.0.1:8000'
+// export const LOCALHOST = 'http://127.0.0.1:8000/'
+// export const IMAGE_HOST = 'http://127.0.0.1:8000'
 // export const LOCALHOST = 'http://94.101.184.56:8000/'
 // export const IMAGE_HOST = 'http://94.101.184.56:8000'
 
-// export const LOCALHOST = 'https://backend.gappy.ir/'
-// export const IMAGE_HOST = 'https://backend.gappy.ir'
+export const LOCALHOST = 'https://backend.gappy.ir/'
+export const IMAGE_HOST = 'https://backend.gappy.ir'
